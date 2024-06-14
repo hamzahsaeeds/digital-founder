ffcc3002b8f187d1c958ac4b83b6ca00b2a67a90
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2024-06-14 23:40:23 +0500

    fixup set main page height to screen height

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,7 +3,7 @@ import Section1 from "./Section1";
 
 export default function MainPage() {
   return (
-    <div className="w-full flex flex-col font-sans">
+    <div className="w-full h-screen flex flex-col font-sans">
       <Header />
       <Section1 />
     </div>
