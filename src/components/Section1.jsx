ab0898d2bbcce819277ca0a42b55690cee3b4f06
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2024-06-14 23:42:25 +0500

    adjusted section1 as per screen size with the help of breakpoints

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -2,24 +2,24 @@ export default function Section1() {
   return (
     <div className="w-full h-screen main-background pt-36">
       <div className="flex flex-col justify-center items-center text-center h-full space-y-12">
-        <span className="font-extrabold text-7xl">
+        <span className="font-extrabold text-3xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-9xl">
           Share your idea! <br />
           we`ll invest in your future
         </span>
         <div className="w-1/2">
-          <span className="font-normal text-xl text-center">
+          <span className="font-normal text-center text-xs md:text-base lg:text-lg xl:text-xl 2xl:text-4xl">
             Have a groundbreaking idea but lack the funds to bring it to life?
             No worries! At Digital Founder, we invest in your vision, putting
             our money on the table to turn your digital idea into reality
           </span>
         </div>
         <div>
-          <button className="flex items-center font-normal text-base text-gray-800 bg-violet-50">
+          <button className="flex items-center font-normal text-gray-800 bg-violet-50 text-xs md:text-base lg:text-lg xl:text-xl 2xl:text-4xl 2xl:w-[340px]">
             Lets Partner
-            <img src="right_arrow.svg" alt="right arrow" />
+            <img src="right_arrow.svg" alt="right arrow" className="pl-1 2xl:size-1/6" />
           </button>
         </div>
-        <div className="cursor-pointer">
+        <div className="cursor-pointer size-5 md:size-7">
           <img src="down_arrow.svg" alt="down arrow" />
         </div>
       </div>
